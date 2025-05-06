70af14a5f7b15385160feac340c6bd2b5d66e075
Orlando Coelho <78504401+orlandocoelhoo@users.noreply.github.com>
2025-05-06 00:16:21 +0000

    criação component heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import styles from "./style.module.css"
+
+type HeadingProps = {
+  children: React.ReactNode
+}
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>
+}
