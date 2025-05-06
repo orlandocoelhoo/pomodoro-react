1cf064c56574f7d839c67cb3fb05202ae9c01d08
Orlando Coelho <78504401+orlandocoelhoo@users.noreply.github.com>
2025-05-06 19:53:54 +0000

    adicionado componentes logo e menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Container } from "./components/Container"
+import { Logo } from "./components/Logo"
+import { Menu } from "./components/Menu"
+
 import "./styles/global.css"
 import "./styles/theme.css"
 
 export function App() {
   return (
     <>
-      <Container>Olá mundo</Container>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
     </>
   )
 }
